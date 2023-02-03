3612eec299452dee2124a6100af49ccf0dfadc29
kisah <data.kisahtegar@gmail.com>
2023-02-03 23:32:10 +0700

    Adding attack feature include sword attack and fireball attack

--- lib/player/knight.dart
+++ lib/player/knight.dart
@@ -1,11 +1,23 @@
+import 'dart:async' as async;
+
 import 'package:bonfire/bonfire.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../main.dart';
 import '../util/const.dart';
+import '../util/sounds.dart';
+import '../util/sprite_sheet/game_sprite_sheet.dart';
 import '../util/sprite_sheet/player_sprite_sheet.dart';
 
 class Knight extends SimplePlayer with Lighting, ObjectCollision {
+  double attack = 25;
+  double stamina = 100;
+  double initSpeed = tileSize / 0.25;
+  async.Timer? _timerStamina;
+  bool containKey = false;
+  bool showObserveEnemy = false;
+
   Knight(Vector2 position)
       : super(
           animation: PlayerSpriteSheet.playerAnimations(),
@@ -33,6 +45,78 @@ class Knight extends SimplePlayer with Lighting, ObjectCollision {
         radius: width * 1.5,
         blurBorder: width,
         color: Colors.deepOrangeAccent.withOpacity(0.2),
+      ),
+    );
+  }
+
+  // Jostick Action while button pressed.
+  @override
+  void joystickAction(JoystickActionEvent event) {
+    if (event.id == 0 && event.event == ActionEvent.DOWN) {
+      actionAttackSword();
+    }
+
+    if (event.id == LogicalKeyboardKey.space.keyId &&
+        event.event == ActionEvent.DOWN) {
+      actionAttackSword();
+    }
+
+    if (event.id == LogicalKeyboardKey.keyZ.keyId &&
+        event.event == ActionEvent.DOWN) {
+      actionAttackRange();
+    }
+
+    if (event.id == 1 && event.event == ActionEvent.DOWN) {
+      actionAttackRange();
+    }
+    super.joystickAction(event);
+  }
+
+  // Player Action Attack with sword
+  void actionAttackSword() {
+    if (stamina < 15) {
+      return;
+    }
+
+    Sounds.attackPlayerMelee();
+    // decrementStamina(15);
+    simpleAttackMelee(
+      damage: attack,
+      animationRight: PlayerSpriteSheet.attackEffectRight(),
+      size: Vector2.all(tileSize),
+    );
+  }
+
+  // Player Action Attack with Skill
+  void actionAttackRange() {
+    if (stamina < 10) {
+      return;
+    }
+
+    Sounds.attackRange();
+
+    simpleAttackRange(
+      animationRight: GameSpriteSheet.fireBallAttackRight(),
+      animationLeft: GameSpriteSheet.fireBallAttackLeft(),
+      animationUp: GameSpriteSheet.fireBallAttackTop(),
+      animationDown: GameSpriteSheet.fireBallAttackBottom(),
+      animationDestroy: GameSpriteSheet.fireBallExplosion(),
+      size: Vector2(tileSize * 0.65, tileSize * 0.65),
+      damage: 10,
+      speed: initSpeed * (tileSize / 32),
+      enableDiagonal: false,
+      onDestroy: () {
+        Sounds.explosion();
+      },
+      collision: CollisionConfig(
+        collisions: [
+          CollisionArea.rectangle(size: Vector2(tileSize / 2, tileSize / 2)),
+        ],
+      ),
+      lightingConfig: LightingConfig(
+        radius: tileSize * 0.9,
+        blurBorder: tileSize / 2,
+        color: Colors.deepOrangeAccent.withOpacity(0.4),
       ),
     );
   }
--- lib/util/sounds.dart
+++ lib/util/sounds.dart
@@ -1,14 +1,29 @@
 import 'package:flame_audio/flame_audio.dart';
 
 class Sounds {
-  static void interaction() {
-    FlameAudio.play('sound_interaction.wav', volume: 0.4);
-  }
-
   static Future initialize() async {
     FlameAudio.bgm.initialize();
     await FlameAudio.audioCache.loadAll([
       'sound_interaction.wav',
+      'attack_player.mp3',
+      'attack_fire_ball.wav',
+      'explosion.wav',
     ]);
+  }
+
+  static void interaction() {
+    FlameAudio.play('sound_interaction.wav', volume: 0.4);
+  }
+
+  static void attackPlayerMelee() {
+    FlameAudio.play('attack_player.mp3', volume: 0.4);
+  }
+
+  static void attackRange() {
+    FlameAudio.play('attack_fire_ball.wav', volume: 0.3);
+  }
+
+  static void explosion() {
+    FlameAudio.play('explosion.wav');
   }
 }
