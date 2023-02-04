ad00dc3e6c1fadf0abfafe3a3a4996ad7fb0f338
kisah <data.kisahtegar@gmail.com>
2023-02-04 12:59:26 +0700

    fixing duplicate code

--- lib/util/sprite_sheet/game_sprite_sheet.dart
+++ lib/util/sprite_sheet/game_sprite_sheet.dart
@@ -11,15 +11,15 @@ class GameSpriteSheet {
         ),
       );
 
-
   static smokeExplosion() => SpriteAnimation.load(
         'smoke_explosin.png',
         SpriteAnimationData.sequenced(
           amount: 6,
           stepTime: 0.1,
           textureSize: Vector2(16, 16),
+        ),
       );
-  
+
   // Fireball attack effect in right position.
   static Future<SpriteAnimation> fireBallAttackRight() => SpriteAnimation.load(
         'player/fireball_right.png',
@@ -61,51 +61,6 @@ class GameSpriteSheet {
       );
 
   // Fireball attack effect exposion.
-  static Future<SpriteAnimation> fireBallExplosion() => SpriteAnimation.load(
-        'player/explosion_fire.png',
-        SpriteAnimationData.sequenced(
-          amount: 6,
-          stepTime: 0.1,
-          textureSize: Vector2(32, 32),
-        ),
-      );
-
-  static fireBallAttackRight() => SpriteAnimation.load(
-        'player/fireball_right.png',
-        SpriteAnimationData.sequenced(
-          amount: 3,
-          stepTime: 0.1,
-          textureSize: Vector2(23, 23),
-        ),
-      );
-
-  static Future<SpriteAnimation> fireBallAttackLeft() => SpriteAnimation.load(
-        'player/fireball_left.png',
-        SpriteAnimationData.sequenced(
-          amount: 3,
-          stepTime: 0.1,
-          textureSize: Vector2(23, 23),
-        ),
-      );
-
-  static Future<SpriteAnimation> fireBallAttackTop() => SpriteAnimation.load(
-        'player/fireball_top.png',
-        SpriteAnimationData.sequenced(
-          amount: 3,
-          stepTime: 0.1,
-          textureSize: Vector2(23, 23),
-        ),
-      );
-
-  static Future<SpriteAnimation> fireBallAttackBottom() => SpriteAnimation.load(
-        'player/fireball_bottom.png',
-        SpriteAnimationData.sequenced(
-          amount: 3,
-          stepTime: 0.1,
-          textureSize: Vector2(23, 23),
-        ),
-      );
-
   static Future<SpriteAnimation> fireBallExplosion() => SpriteAnimation.load(
         'player/explosion_fire.png',
         SpriteAnimationData.sequenced(
