c3098be5dc29adf5967e92385066eba612b59084
Aldi Maulana Bahari <aldimaulana1605.com>
2023-02-18 08:54:05 +0700

    adding : map2

---
+++ lib/decoration/barrel.dart
@@ -0,0 +1,23 @@
+import 'package:bonfire/bonfire.dart';
+
+import '../main.dart';
+
+class Barrel extends GameDecoration with ObjectCollision {
+  Barrel(Vector2 position)
+      : super.withSprite(
+          sprite: Sprite.load('items/barrel.png'),
+          position: position,
+          size: Vector2(tileSize, tileSize),
+        ) {
+    setupCollision(
+      CollisionConfig(
+        collisions: [
+          CollisionArea.rectangle(
+            size: Vector2(tileSize * 0.6, tileSize * 0.6),
+            align: Vector2(tileSize * 0.2, 0),
+          ),
+        ],
+      ),
+    );
+  }
+}
